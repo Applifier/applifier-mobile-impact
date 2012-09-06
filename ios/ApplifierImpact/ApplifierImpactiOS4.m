e2aba7c69681ba06d12dfdf8b35233dce91733c7
Johan Halin <johan.halin@karppinen.fi>
2012-09-06 14:29:06 +0300

    Operate the campaign manager in a background thread.

--- ios/ApplifierImpact/ApplifierImpactiOS4.m
+++ ios/ApplifierImpact/ApplifierImpactiOS4.m
@@ -11,6 +11,7 @@
 
 @interface ApplifierImpact ()
 @property (nonatomic, strong) NSString *applifierID;
+@property (nonatomic, strong) NSThread *backgroundThread;
 @property (nonatomic, strong) ApplifierImpactCampaignManager *campaignManager;
 @property (nonatomic, strong) UIWindow *applifierWindow;
 @property (nonatomic, strong) UIWebView *webView;
@@ -19,9 +20,35 @@
 @implementation ApplifierImpactiOS4
 
 @synthesize applifierID = _applifierID;
+@synthesize backgroundThread = _backgroundThread;
 @synthesize campaignManager = _campaignManager;
 @synthesize applifierWindow = _applifierWindow;
 @synthesize webView = _webView;
+
+#pragma mark - Private
+
+- (void)_backgroundRunLoop:(id)dummy
+{
+	@autoreleasepool
+	{
+		NSPort *port = [[NSPort alloc] init];
+		[port scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+		
+		while([[NSThread currentThread] isCancelled] == NO)
+		{
+			@autoreleasepool
+			{
+				[[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:3.0]];
+			}
+		}
+	}
+}
+
+- (void)_startCampaignManager
+{
+	self.campaignManager = [[ApplifierImpactCampaignManager alloc] init];
+	[self.campaignManager updateCampaigns];
+}
 
 #pragma mark - Public
 
@@ -31,9 +58,10 @@
 		return;
 	
 	self.applifierID = applifierID;
-	self.campaignManager = [[ApplifierImpactCampaignManager alloc] init];
-
-	[self.campaignManager updateCampaigns];
+	self.backgroundThread = [[NSThread alloc] initWithTarget:self selector:@selector(_backgroundRunLoop:) object:nil];
+	[self.backgroundThread start];
+	
+	[self performSelector:@selector(_startCampaignManager) onThread:self.backgroundThread withObject:nil waitUntilDone:NO];
 }
 
 - (BOOL)showImpact
