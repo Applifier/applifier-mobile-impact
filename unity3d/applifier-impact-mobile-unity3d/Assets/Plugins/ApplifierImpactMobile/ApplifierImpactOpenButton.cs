d5532f1dd08d7756aa8cb30215b3838c8cb46a1e
Pekka Palmu <pekka.palmu@gmail.com>
2013-02-28 17:10:08 +0200

    Cleanup code

--- unity3d/applifier-impact-mobile-unity3d/Assets/Plugins/ApplifierImpactMobile/ApplifierImpactOpenButton.cs
+++ unity3d/applifier-impact-mobile-unity3d/Assets/Plugins/ApplifierImpactMobile/ApplifierImpactOpenButton.cs
@@ -5,14 +5,6 @@ using System.Collections.Generic;
 public class ApplifierImpactOpenButton : MonoBehaviour {
 	private bool _canOpenImpact = false;
 	
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
-	
 	void OnGUI () {
 		if (ApplifierImpactMobile.getTestButtonVisibility()) {
 			string buttonText = "Waiting...";
@@ -27,22 +19,6 @@ public class ApplifierImpactOpenButton : MonoBehaviour {
 				if (_canOpenImpact) {
 					ApplifierImpactMobileExternal.Log("Open Impact -button clicked");
 					ApplifierImpactMobile.showImpact();
-				}	
-			}
-			
-			if (GUI.Button (new Rect (10,70,170,50), "Show rewardItemKeys")) {
-				if (_canOpenImpact) {
-					List<string> keys = ApplifierImpactMobile.getRewardItemKeys();
-					foreach (string key in keys) {
-						ApplifierImpactMobileExternal.Log("Reward key: " + key);
-					}
-				}	
-			}
-			
-			if (GUI.Button (new Rect (10,130,170,50), "Reward item details")) {
-				if (_canOpenImpact) {
-					ApplifierImpactMobileExternal.Log("Trying to fetch details with key: " + ApplifierImpactMobile.getCurrentRewardItemKey());
-					ApplifierImpactMobile.getRewardItemDetailsWithKey(ApplifierImpactMobile.getCurrentRewardItemKey());
 				}	
 			}
 		}
