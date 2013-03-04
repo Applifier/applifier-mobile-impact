6d8dba66dbdbe2f58f3e8c2a69ae9a4e46bbd3d9
Tuomas Rinta <trinta@gmail.com>
2013-03-04 14:58:25 +0200

    Initial Burstly adaptor for Android

---
+++ burstly/android/src/com/applifier/impact/android/burstly/ImpactAdaptorFactory.java
@@ -0,0 +1,58 @@
+package com.applifier.impact.android.burstly;
+
+import java.util.Map;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.burstly.lib.component.IBurstlyAdaptor;
+import com.burstly.lib.feature.networks.IAdaptorFactory;
+
+/**
+ * Adaptor for Impact
+ * 
+ * @author tuomasrinta
+ *
+ */
+public class ImpactAdaptorFactory implements IAdaptorFactory {
+	
+	static {
+		Log.d("burstly_applifier", "Initializing class ImpactAdaptorFactory");
+	}
+
+    /**
+     * A key for context object being passed in parameters.
+     */
+    private static final String CONTEXT = "context";
+ 
+    /**
+     * A key for current BurstlyView id object being passed in parameters.
+     */
+    private static final String VIEW_ID = "viewId";
+
+    /**
+     * A key for adaptor name being passed in parameters.
+     */
+    private static final String ADAPTOR_NAME = "adaptorName";
+    
+
+	@Override
+	public IBurstlyAdaptor createAdaptor(Map<String, ?> params) {
+		
+		Log.d("burstly_applifier", "Creating adaptor w/ Context:" + params.get(ImpactAdaptorFactory.CONTEXT).getClass().getName());
+		
+		return new ImpactAdaptor(
+				(Context)params.get(ImpactAdaptorFactory.CONTEXT)
+			);
+		
+	}
+
+	@Override
+	public void destroy() {}
+
+	@Override
+	public void initialize(Map<String, ?> arg0) throws IllegalArgumentException {
+		// Since we don't have the Game ID here, we can't init the adaptor yet
+	}
+
+}
