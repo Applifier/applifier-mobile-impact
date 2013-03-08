f51ce87f303d7544d624642825d007d04ae872e1
Pekka Palmu <pekka.palmu@gmail.com>
2013-03-08 13:55:05 +0200

    Files for Unity3d ios wrapper

---
+++ ios/ApplifierImpactUnity/ApplifierImpactUnity/ApplifierImpactUnity3DWrapper.h
@@ -0,0 +1,13 @@
+//
+//  ApplifierImpactUnity3DWrapper.h
+//  ApplifierImpactUnity
+//
+//  Created by Pekka Palmu on 3/8/13.
+//  Copyright (c) 2013 Pekka Palmu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ApplifierImpactUnity3DWrapper : NSObject
+
+@end
---
+++ ios/ApplifierImpactUnity/ApplifierImpactUnity/ApplifierImpactUnity3DWrapper.m
@@ -0,0 +1,13 @@
+//
+//  ApplifierImpactUnity3DWrapper.m
+//  ApplifierImpactUnity
+//
+//  Created by Pekka Palmu on 3/8/13.
+//  Copyright (c) 2013 Pekka Palmu. All rights reserved.
+//
+
+#import "ApplifierImpactUnity3DWrapper.h"
+
+@implementation ApplifierImpactUnity3DWrapper
+
+@end
