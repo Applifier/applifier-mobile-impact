4d47aa04424c3b6bc8984af33400ae8138edba79
Matti Savolainen <matti@applifier.com>
2013-05-06 13:42:31 +0300

    fix simulator build

---
+++ ios/ApplifierImpact/ApplifierImpactVideo/ApplifierImpactVideoOverlay.h
@@ -0,0 +1,13 @@
+//
+//  ApplifierImpactVideoOverlay.h
+//  ApplifierImpact
+//
+//  Created by Matti Savolainen on 5/6/13.
+//  Copyright (c) 2013 Applifier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApplifierImpactVideoOverlay : UIView
+
+@end
---
+++ ios/ApplifierImpact/ApplifierImpactVideo/ApplifierImpactVideoOverlay.m
@@ -0,0 +1,23 @@
+//
+//  ApplifierImpactVideoOverlay.m
+//  ApplifierImpact
+//
+//  Created by Matti Savolainen on 5/6/13.
+//  Copyright (c) 2013 Applifier. All rights reserved.
+//
+
+#import "ApplifierImpactVideoOverlay.h"
+
+@implementation ApplifierImpactVideoOverlay
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+@end
