4758b43c26d930dd8588c70dcc5bc6f3a49f901f
Ville Orkas <ville.orkas@applifier.com>
2013-08-22 11:02:11 +0300

    Remove OpenUDID from iOS 7

--- ios/ApplifierImpact/ApplifierImpactProperties/ApplifierImpactProperties.m
+++ ios/ApplifierImpact/ApplifierImpactProperties/ApplifierImpactProperties.m
@@ -53,9 +53,9 @@ static ApplifierImpactProperties *sharedImpactProperties = nil;
   queryParams = [NSString stringWithFormat:@"%@%@=%@", queryParams, kApplifierImpactInitQueryParamPlatformKey, @"ios"];
   queryParams = [NSString stringWithFormat:@"%@&%@=%@", queryParams, kApplifierImpactInitQueryParamGameIdKey, [self impactGameId]];
   queryParams = [NSString stringWithFormat:@"%@&%@=%@", queryParams, kApplifierImpactInitQueryParamSdkVersionKey, kApplifierImpactVersion];
-  queryParams = [NSString stringWithFormat:@"%@&%@=%@", queryParams, kApplifierImpactInitQueryParamOpenUdidKey, [ApplifierImpactDevice md5OpenUDIDString]];
   
   if ([ApplifierImpactDevice getIOSMajorVersion] < 7) {
+    queryParams = [NSString stringWithFormat:@"%@&%@=%@", queryParams, kApplifierImpactInitQueryParamOpenUdidKey, [ApplifierImpactDevice md5OpenUDIDString]];
     queryParams = [NSString stringWithFormat:@"%@&%@=%@", queryParams, kApplifierImpactInitQueryParamMacAddressKey, [ApplifierImpactDevice md5MACAddressString]];
     if ([ApplifierImpactDevice ODIN1] != nil) {
       queryParams = [NSString stringWithFormat:@"%@&%@=%@", queryParams, kApplifierImpactInitQueryParamOdin1IdKey, [ApplifierImpactDevice ODIN1]];
