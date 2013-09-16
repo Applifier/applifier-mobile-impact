2c053f04a2f2b58ae0bb7375ffa769ccf4940257
Ville Orkas <ville.orkas@applifier.com>
2013-09-16 11:27:23 +0300

    NaN checks

--- ios/ApplifierImpact/ApplifierImpactView/ApplifierImpactImageView.m
+++ ios/ApplifierImpact/ApplifierImpactView/ApplifierImpactImageView.m
@@ -105,8 +105,12 @@
     
     //[self setTransform:CGAffineTransformMakeTranslation(0, -(109 / 2))];
     
-    [self setFrame:newPos];
-    [self setNeedsLayout];
+    if(isnan(newPos.origin.x) || isnan(newPos.origin.y) || isnan(newPos.size.width) || isnan(newPos.size.height)) {
+      AILOG_DEBUG(@"Invalid frame rectangle");
+    } else {
+      [self setFrame:newPos];
+      [self setNeedsLayout];
+    }
   }
   
   self.data = nil;
