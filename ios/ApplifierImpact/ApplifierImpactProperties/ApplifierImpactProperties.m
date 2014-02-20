6ac5693dbb0ddad51c9b35410b5b1282d8f70632
Ville Orkas <ville.orkas@applifier.com>
2014-02-20 12:06:13 +0200

    Switch default url to production

--- ios/ApplifierImpact/ApplifierImpactProperties/ApplifierImpactProperties.m
+++ ios/ApplifierImpact/ApplifierImpactProperties/ApplifierImpactProperties.m
@@ -29,8 +29,8 @@ static ApplifierImpactProperties *sharedImpactProperties = nil;
 - (ApplifierImpactProperties *)init {
   if (self = [super init]) {
     [self setMaxNumberOfAnalyticsRetries:5];
-    //[self setCampaignDataUrl:@"https://impact.applifier.com/mobile/campaigns"];
-    [self setCampaignDataUrl:@"https://staging-impact.applifier.com/mobile/campaigns"];
+    [self setCampaignDataUrl:@"https://impact.applifier.com/mobile/campaigns"];
+    //[self setCampaignDataUrl:@"https://staging-impact.applifier.com/mobile/campaigns"];
     //[self setCampaignDataUrl:@"http://192.168.1.246:3500/mobile/campaigns"];
     [self setCampaignQueryString:[self _createCampaignQueryString]];
     [self setSdkIsCurrent:true];
