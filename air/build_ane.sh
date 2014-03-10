2504adee3cc201bd6f2ff4ca65ebb53cd45c3444
Ville Orkas <ville.orkas@applifier.com>
2014-03-10 11:45:01 +0200

    Remove support for Adobe AIR

--- air/build_ane.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd build/android
-jar -xf ../../../android/sources/jar/applifier-impact-android.jar
-jar -uf libApplifierImpactMobileAIRWrapper.jar com
-rm -rf com META-INF
-cd ..
-~/AIRSDK_Compiler/bin/adt -package -target ane ../native_extension/applifier-impact-mobile.ane extension.xml -swc ./ApplifierImpactMobileAIRWrapper.swc -platform iPhone-ARM -C ios libApplifierImpactMobileAIRWrapper.a library.swf ApplifierImpact.bundle -platformoptions platformoptions_ios.xml -platform Android-ARM -C android . -platform default -C default .
