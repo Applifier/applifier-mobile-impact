e8f1a978b055acd5d9aac0caa377273b569c4fd4
Sergey D <sergey.dankevich@applifier.com>
2014-03-11 16:17:30 +0200

    compiling tests

--- ios/ApplifierImpactTests/ApplifierImpactCacheManagerTests.m
+++ ios/ApplifierImpactTests/ApplifierImpactCacheManagerTests.m
@@ -6,11 +6,13 @@
 //  Copyright (c) 2014 Applifier. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import <SenTestingKit/SenTestingKit.h>
 
-@interface ApplifierImpactCacheManagerTests : XCTestCase
+@interface ApplifierImpactCacheManagerTests : SenTestCase
 
 @end
+
+extern void __gcov_flush();
 
 @implementation ApplifierImpactCacheManagerTests
 
@@ -20,15 +22,10 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)tearDown {
+  __gcov_flush();
+  [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
 
 @end
