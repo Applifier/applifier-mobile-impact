8bc231241fc1fa4478b559aa2ce8782e05769d76
Sergey D <sergey.dankevich@applifier.com>
2014-03-11 18:07:21 +0200

    caching manager tests
    testCacheEmptyCampaign added

--- ios/ApplifierImpactTests/ApplifierImpactCacheManagerTests.m
+++ ios/ApplifierImpactTests/ApplifierImpactCacheManagerTests.m
@@ -10,13 +10,36 @@
 #import "ApplifierImpactCampaign.h"
 #import "ApplifierImpactCacheManager.h"
 
-@interface ApplifierImpactCacheManagerTests : SenTestCase <ApplifierImpactCacheManagerDelegate>
+typedef enum {
+  CachingResultUndefined = 0,
+  CachingResultFinished,
+  CachingResultFailed,
+  CachingResultCancelled,
+} CachingResult;
+
+@interface ApplifierImpactCacheManagerTests : SenTestCase <ApplifierImpactCacheManagerDelegate> {
+  @private
+  CachingResult cachingResult;
+}
 
 @end
 
 extern void __gcov_flush();
 
 @implementation ApplifierImpactCacheManagerTests
+
+- (void)threadBlocked:(BOOL (^)())isThreadBlocked {
+	@autoreleasepool {
+		NSPort *port = [[NSPort alloc] init];
+		[port scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+		
+		while(isThreadBlocked) {
+			@autoreleasepool {
+				[[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+			}
+		}
+	}
+}
 
 - (void)setUp
 {
@@ -30,17 +53,30 @@ extern void __gcov_flush();
 }
 
 - (void)testCacheEmptyCampaign {
+  cachingResult = CachingResultUndefined;
   ApplifierImpactCacheManager * cacheManager = [ApplifierImpactCacheManager new];
   ApplifierImpactCampaign * campaignToCache = [ApplifierImpactCampaign new];
   [cacheManager cacheCampaign:campaignToCache];
+  STAssertTrue(cachingResult == CachingResultFailed,
+               @"caching should fail instantly in same thread when caching empty campaign");
 }
 
 - (void)cache:(ApplifierImpactCacheManager *)cache failedToCacheCampaign:(ApplifierImpactCampaign *)campaign {
-  
+  @synchronized(self) {
+    cachingResult = CachingResultFailed;
+  }
 }
 
 - (void)cache:(ApplifierImpactCacheManager *)cache finishedCachingCampaign:(ApplifierImpactCampaign *)campaign {
-  
+  @synchronized(self) {
+    cachingResult = CachingResultFinished;
+  }
+}
+
+- (void)cache:(ApplifierImpactCacheManager *)cache cancelledCaching:(ApplifierImpactCampaign *)campaign {
+  @synchronized(self) {
+    cachingResult = CachingResultCancelled;
+  }
 }
 
 @end
